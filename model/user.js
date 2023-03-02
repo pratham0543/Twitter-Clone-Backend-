3a48800715220cae027b3e5a7d17ccd22cef4db1
Manvendra <78345394+manvendrarc@users.noreply.github.com>
2023-03-02 09:34:06 +0530

    Added User Schema

---
+++ model/user.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+
+const User = new mongoose.Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true
+    },
+    name: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    username: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: mongoose.Schema.Types.String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    user_type: {
+        type: mongoose.Schema.Types.String,
+        required: true
+    },
+    phone: {
+        type: mongoose.Schema.Types.Number,
+        required: true,
+        unique: true
+    },
+    dob: {
+        type: mongoose.Schema.Types.Date,
+        required: true
+    },
+    gender: {
+        type: mongoose.Schema.Types.String,
+        default: ""
+    },
+    location: {
+        type: mongoose.Schema.Types.String,
+        default: ""
+    }
+})
+
+module.exports = mongoose.model("UserInfo", User)
