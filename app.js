b75daafacd07b7d3ddd2cc6d088d130c13bb5f08
Pranav Jindal <71919420+Pranav-github02@users.noreply.github.com>
2023-03-08 16:42:04 +0530

    updated app.js and added handling of routes

--- app.js
+++ app.js
@@ -1,29 +1,35 @@
-const express=require('express');
-const app=express();
-const cors=require('cors')
-const bodyParser=require('body-parser')
-const dotenv=require('dotenv')
-const mongoose=require('mongoose')
-dotenv.config({path:'./.env'})
-app.use(cors());
-app.use(bodyParser.urlencoded({extended:false}))
-app.use(bodyParser.json());
-const url=process.env.URL
-mongoose.set('strictQuery',true)
+const express = require("express")
+const app = express()
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const dotenv = require("dotenv");
+dotenv.config({ path: "./.env" });
+
+const signup = require("./routes/signup")
+const login = require('./routes/login')
+const tweet = require('./routes/tweet')
+const createAdmin = require('./routes/admin')
+
+app.use(cors())
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+
+const url = process.env.URL
+
+mongoose.set('strictQuery', true)
 mongoose.connect(url)
     .then(console.log('DB Connection successful!'))
     .catch(err => console.log(err))
 
+app.use('/signup', signup)
+app.use('/login', login)
+app.use('/tweet', tweet)
+app.use('/createAdmin', createAdmin)
 
+app.get('/', (req, res) => {
+    res.status(200).json({ msg: 'Resource not found' })
+})
 
-
-
-
-app.use('/',(req,res)=>res.status(200).json({msg:"Resource not found"}))
-
-
-
-module.exports=app;
-
-
-
+module.exports = app
